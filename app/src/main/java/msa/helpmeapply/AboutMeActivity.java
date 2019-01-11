367ea20d659c5a86882678bf0f712a15777f8e4f
Mihai Cania <mihai.cania@gmail.com>
2019-01-11 21:57:13 +0200

    Step1:Menu

--- app/src/main/java/msa/helpmeapply/AboutMeActivity.java
+++ app/src/main/java/msa/helpmeapply/AboutMeActivity.java
@@ -1,17 +1,17 @@
 package msa.helpmeapply;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class AboutMeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_me);
     }
 
     @Override
@@ -25,18 +25,19 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if(id == R.id.aboutme) {
-            Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
+            Intent intent = new Intent(AboutMeActivity.this, AboutMeActivity.class);
             startActivity(intent);
             return false;
         }
 
         if(id == R.id.companies) {
-            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            Intent intent = new Intent(AboutMeActivity.this, MainActivity.class);
             startActivity(intent);
             return false;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 
 }
--- app/src/main/java/msa/helpmeapply/SignInActivity.java
+++
@@ -1,8 +0,0 @@
-package msa.helpmeapply;
-
-
-import android.support.v7.app.AppCompatActivity;
-
-public class SignInActivity extends AppCompatActivity {
-
-}
