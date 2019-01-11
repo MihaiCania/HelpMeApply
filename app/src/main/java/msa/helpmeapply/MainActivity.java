0271f879e3bb3f0a84a1dc7ab2e30484a60c1d6a
Mihai Cania <mihai.cania@gmail.com>
2019-01-12 01:50:53 +0200

    Firebase Database Working

--- app/src/main/java/msa/helpmeapply/MainActivity.java
+++ app/src/main/java/msa/helpmeapply/MainActivity.java
@@ -2,17 +2,180 @@ package msa.helpmeapply;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView dataListView;
+    private EditText itemText;
+    private Button findButton;
+    private Button deleteButton;
+    private Boolean searchMode = false;
+    private Boolean itemSelected = false;
+    private int selectedPosition = 0;
+
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayList<String> listKeys = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference dbRef = database.getReference("todo");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataListView = (ListView) findViewById(R.id.dataListView);
+        itemText = (EditText) findViewById(R.id.itemText);
+        findButton = (Button) findViewById(R.id.findButton);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
+        deleteButton.setEnabled(false);
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_single_choice,
+                listItems);
+        dataListView.setAdapter(adapter);
+        dataListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        dataListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent,
+                                            View view, int position, long id) {
+                        selectedPosition = position;
+                        itemSelected = true;
+                        deleteButton.setEnabled(true);
+                    }
+                });
+
+        addChildEventListener();
     }
+
+    private void addChildEventListener() {
+        ChildEventListener childListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                adapter.add(
+                        (String) dataSnapshot.child("description").getValue());
+
+                listKeys.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                String key = dataSnapshot.getKey();
+                int index = listKeys.indexOf(key);
+
+                if(index != -1) {
+                    listItems.remove(index);
+                    listKeys.remove(index);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        dbRef.addChildEventListener(childListener);
+    }
+
+    public void addItem(View view) {
+        String item = itemText.getText().toString();
+        String key = dbRef.push().getKey();
+
+        itemText.setText("");
+        dbRef.child(key).child("description").setValue(item);
+
+        adapter.notifyDataSetChanged();
+    }
+
+    public void deleteItem(View view) {
+        dataListView.setItemChecked(selectedPosition, false);
+        dbRef.child(listKeys.get(selectedPosition)).removeValue();
+    }
+
+    public void findItems(View view) {
+
+        Query query;
+
+        if (!searchMode) {
+            findButton.setText("Clear");
+            query = dbRef.orderByChild("description").
+                    equalTo(itemText.getText().toString());
+            searchMode = true;
+        } else {
+            searchMode = false;
+            findButton.setText("Find");
+            query = dbRef.orderByKey();
+        }
+
+        if (itemSelected) {
+            dataListView.setItemChecked(selectedPosition, false);
+            itemSelected = false;
+            deleteButton.setEnabled(false);
+        }
+
+        query.addListenerForSingleValueEvent(queryValueListener);
+    }
+
+    ValueEventListener queryValueListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
+            Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
+
+            adapter.clear();
+            listKeys.clear();
+
+            while(iterator.hasNext()) {
+                DataSnapshot next = (DataSnapshot) iterator.next();
+
+                String match = (String) next.child("description").getValue();
+                String key = next.getKey();
+                listKeys.add(key);
+                adapter.add(match);
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
